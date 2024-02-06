1d08fc5cd6c30899d0c9f775027c19fcd9e62cbf
Tejeswar <tejeswarreddygali@gmail.com>
2024-02-06 15:10:25 +0530

    created sample.txt file and read all the data form sample.txt to file_read.py file

---
+++ Source_code/file_read.py
@@ -0,0 +1,3 @@
+with open(r'C:\Users\Naveen Reddy\PycharmProjects\pythonProject\pythonProject\pythonProject\ust_test\Source_code\Sub_dir\sample.txt','r') as file:
+    content = file.read()
+    print(content)
