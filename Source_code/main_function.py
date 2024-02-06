7fab5cc5edb4e47040795319c2fc72e405ead4ca
Tejeswar <tejeswarreddygali@gmail.com>
2024-02-06 14:46:22 +0530

    created a multi functions and calling in main function

---
+++ Source_code/main_function.py
@@ -0,0 +1,14 @@
+from multi_functions import *
+
+def main():
+    print("calling all function")
+
+    first_function_output=first_function(5)
+    second_function_output = second_function(2)
+    third_function_output= third_function(3)
+
+    print("output of first function",first_function_output)
+    print("output of second function",second_function_output)
+    print("output of third function",third_function_output)
+
+main()
---
+++ Source_code/multi_functions.py
@@ -0,0 +1,9 @@
+def first_function(x):
+    return x+2
+
+def second_function(y):
+    return y*2
+
+def third_function(z):
+    return z+5
+
